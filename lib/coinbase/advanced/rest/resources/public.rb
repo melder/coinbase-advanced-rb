c637f9c5394c73354bdddd3cdd67c036e9111d29
Michael Elder <eldertree@gmail.com>
2024-12-05 10:04:46 -0800

    Implements public endpoints

--- lib/coinbase/advanced/rest/resources/public.rb
+++ lib/coinbase/advanced/rest/resources/public.rb
@@ -5,8 +5,45 @@ module Coinbase
     module REST
       module Resources
         module Public
-          def get_unix_time(params = {})
+          def server_time
             endpoint = "#{Coinbase::Advanced::API_PREFIX}/time"
+            get(endpoint, {}, auth_required: false)
+          end
+
+          def public_product_book(params = {})
+            raise ArgumentError, "Missing :product_id" unless params.key? :product_id
+
+            endpoint = "#{Coinbase::Advanced::API_PREFIX}/market/product_book"
+            get(endpoint, params, auth_required: false)
+          end
+
+          def list_public_products(params = {})
+            endpoint = "#{Coinbase::Advanced::API_PREFIX}/market/products"
+            get(endpoint, params, auth_required: false)
+          end
+
+          def public_product(product_id, params = {})
+            raise ArgumentError, "Missing product_id" if product_id.empty?
+
+            endpoint = "#{Coinbase::Advanced::API_PREFIX}/market/products/#{product_id}"
+            get(endpoint, params, auth_required: false)
+          end
+
+          def public_product_candles(product_id, params = {})
+            raise ArgumentError, "Missing product_id" if product_id.empty?
+            raise ArgumentError, "Missing :start" unless params.key? :start
+            raise ArgumentError, "Missing :end" unless params.key? :end
+            raise ArgumentError, "Missing :granularity" unless params.key? :granularity
+
+            endpoint = "#{Coinbase::Advanced::API_PREFIX}/market/products/#{product_id}/candles"
+            get(endpoint, params, auth_required: false)
+          end
+
+          def public_market_trades(product_id, params = {})
+            raise ArgumentError, "Missing product_id" if product_id.empty?
+            raise ArgumentError, "Missing :limit" unless params.key? :limit
+
+            endpoint = "#{Coinbase::Advanced::API_PREFIX}/market/products/#{product_id}/ticker"
             get(endpoint, params, auth_required: false)
           end
         end
